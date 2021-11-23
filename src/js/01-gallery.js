352fa556a41af5ea5442b3500147abf68dfcae63
Kilkenni <hpittman89@outlook.com>
2021-11-23 21:33:17 +0200

    part 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+let galleryMarkup = "";
+
+galleryItems.forEach((galleryItem) => {
+    //do magic
+    const itemMarkup = `<a class="gallery__item" href=${galleryItem.original}>
+        <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}"
+        />
+    </a>`;
+
+    galleryMarkup += itemMarkup;
+});
+
+const galleryElem = document.querySelector(".gallery");
+
+galleryElem.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const modalImage = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay:250, overlay:true });
