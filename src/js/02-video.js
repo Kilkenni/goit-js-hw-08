dd42da2ffa2df2da29e976af2740c517884fe997
Kilkenni <hpittman89@outlook.com>
2021-11-23 22:50:39 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+const video = document.getElementById("vimeo-player");
 
+const player = new Vimeo.Player(video);
+
+if(localStorage.getItem("videoplayer-current-time") !== null) {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+}
+
+const throttle = require('lodash.throttle');
+
+function saveCurrentTime(data) {
+    //do stuff
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+    console.log(localStorage.getItem("videoplayer-current-time"));
+}
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
